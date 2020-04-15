b9e64258388d4f65c6017c8e2905677e0b9e115f
Anton Nedogarok <nk260an@gmail.com>
2020-04-15 20:28:03 +0300

    Corrected VNC script

--- XFCE4_through_VNC.sh
+++ XFCE4_through_VNC.sh
@@ -5,7 +5,7 @@ sudo apt-get install xfce4 tightvncserver
 cp -r XFCE4_through_VNC/.vnc/ ~/
 # cp -r XFCE4_through_VNC/startVNC.sh ~/
 sudo chmod +x ~/.vnc/xstartup
-crontab -e
-sudo apt-get remove openssh-server
-sudo apt-get install openssh-server
+#crontab -e
+#sudo apt-get remove openssh-server
+#sudo apt-get install openssh-server
 vncserver :1 -geometry 1200x700 -localhost
